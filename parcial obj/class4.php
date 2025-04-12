f9e0a477656bd8fe14d9936e4f35d800cc63b765
ailin-franquez <maria.franquez.est.fi.uncoma.edu.ar>
2025-04-12 19:29:40 -0300

    a

---
+++ parcial obj/class4.php
@@ -0,0 +1,67 @@
+<?php class obj{
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+
+    public function __construct($ ,$ ,$ ,$ ,$ ,$ ,$ ){
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function get (){
+        return $this-> ;
+    }public function set ($var){
+        $this-> =$var;
+    }
+
+    public function __toString(){
+
+        return ;
+    }
+}
+?>
