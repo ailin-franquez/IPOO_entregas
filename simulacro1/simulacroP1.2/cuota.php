28ddb2105afd729775f474222dd1b8bd09e403c0
ailin-franquez <maria.franquez.est.fi.uncoma.edu.ar>
2025-04-14 16:51:59 -0300

    matenme

---
+++ simulacro1/simulacroP1.2/cuota.php
@@ -0,0 +1,74 @@
+<?php 
+class cuota{
+    private $numero;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+    private $ ;
+
+    public function __construct($numero,$ ,$ ,$ ,$ ,$ ,$ ){
+        $this->numero=$numero;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+        $this-> =$ ;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function get_(){
+        return $this-> ;
+    }public function set_($var){
+        $this-> =$var;
+    }
+
+    public function __toString(){
+        $array=$this->get_();
+        foreach($array as $indice){
+            $cadena.=$indice."\n";
+        }
+
+        $tex="\n:".$this->get_(). ;
+        $tex.="\n:".$this->get_().;
+        return $tex;
+    }
+}
+?>
