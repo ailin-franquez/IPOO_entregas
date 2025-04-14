0f9c611680debdc6c30862e57a4805c8d88badad
zakku <maria.franquez@est.fi.uncoma.edu.ar>
2025-04-14 18:15:44 -0300

    Update cuota.php

--- simulacro1/simulacroP1.2/cuota.php
+++ simulacro1/simulacroP1.2/cuota.php
@@ -1,74 +1,61 @@
 <?php 
 class cuota{
     private $numero;
-    private $ ;
-    private $ ;
-    private $ ;
-    private $ ;
-    private $ ;
-    private $ ;
+    private $monto_cuota;
+    private $monto_interes;
+    private $cuota_pagada;//bool, si la cuota esta pagada o no
+    //originalmente $cancelada
 
-    public function __construct($numero,$ ,$ ,$ ,$ ,$ ,$ ){
+    public function __construct($numero,$monto_cuota,$monto_interes,$cuota_pagada){
         $this->numero=$numero;
-        $this-> =$ ;
-        $this-> =$ ;
-        $this-> =$ ;
-        $this-> =$ ;
-        $this-> =$ ;
-        $this-> =$ ;
+        $this->monto_cuota=$monto_cuota;
+        $this->monto_interes=$monto_interes;
+        $this->cuota_pagada=$cuota_pagada;
     }
 
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
+    public function get_numero(){
+        return $this->numero;
+    }public function set_numero($var){
+        $this->numero=$var;
     }
 
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
+    public function get_monto_cuota(){
+        return $this->monto_cuota;
+    }public function set_monto_cuota($var){
+        $this->monto_cuota=$var;
     }
 
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
+    public function get_monto_interes(){
+        return $this->monto_interes;
+    }public function set_monto_interes($var){
+        $this->monto_interes=$var;
     }
 
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
+    public function get_cuota_pagada(){
+        return $this->cuota_pagada;
+    }public function set_cuota_pagada($var){
+        $this->cuota_pagada=$var;
     }
 
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
-    }
-
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
-    }
-
-    public function get_(){
-        return $this-> ;
-    }public function set_($var){
-        $this-> =$var;
-    }
 
     public function __toString(){
-        $array=$this->get_();
-        foreach($array as $indice){
-            $cadena.=$indice."\n";
+        $bool=$this->get_cuota_pagada();
+        if($bool){
+            $cuotaPagada="pagada";
+        }else{
+            $cuotaPagada="cancelada";
         }
 
-        $tex="\n:".$this->get_(). ;
-        $tex.="\n:".$this->get_().;
+        $tex="\nnumero:".$this->get_numero().="\nmonto cuota:".$this->get_monto_cuota();
+        $tex.="\nmonto interes:".$this->get_monto_interes().="\ncuota pagada:".$cuotaPagada;
         return $tex;
+    }
+
+    /** retorna el importe total de la cuota mas los intereses 
+    * que deben ser aplicados
+    */
+    public function darMontoFinal(){
+        
     }
 }
 ?>
